a41b98fba1f6aacd9e7896da31b8e19cbfa50c3e
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2026-03-22 03:30:08 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,163 @@
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
+vim.g.netrw_altv = 1
+vim.g.netrw_browse_split = 4
+vim.g.netrw_liststyle = 3
+vim.g.netrw_preview = 1
+vim.g.netrw_winsize = 16
+
+vim.opt.autoindent = true
+vim.opt.background = 'dark'
+vim.opt.backspace = { 'indent', 'eol', 'start', 'nostop' }
+vim.opt.backup = false
+vim.opt.breakindent = true
+vim.opt.clipboard = 'unnamedplus' -- Sync with system clipboard
+-- vim.opt.cmdheight = 0
+vim.opt.colorcolumn = '120'
+vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'popup', 'fuzzy', 'nosort' }
+vim.opt.confirm = true
+vim.opt.copyindent = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = 'number'
+vim.opt.diffopt = { 'internal', 'filler', 'closeoff', 'algorithm:histogram', 'linematch:60' }
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.fileformat = 'unix'
+vim.opt.fileformats = { 'unix', 'dos' }
+vim.opt.fillchars = {
+  -- fold = '·',
+  foldopen = '󰅀',
+  foldclose = '󰅂',
+  -- foldsep = ' ',
+  diff = ' ',
+  eob = ' ', -- disable `~` on nonexistent lines
+}
+vim.opt.foldcolumn = '1'
+vim.opt.foldenable = true
+-- vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldlevel = 99
+-- vim.opt.foldmethod = 'expr'
+vim.opt.foldtext = ''
+vim.opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+-- vim.opt.inccommand = 'split'
+vim.opt.incsearch = true
+vim.opt.infercase = true
+vim.opt.jumpoptions = { 'stack', 'view', 'clean' }
+vim.opt.laststatus = 3
+vim.opt.mouse = 'a'
+vim.opt.mousemoveevent = true
+vim.opt.number = true
+vim.opt.numberwidth = 1
+vim.opt.path = '.,,**'
+vim.opt.pumheight = 16
+vim.opt.relativenumber = true
+vim.opt.ruler = false
+vim.opt.scrolloff = 4
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 0 -- number of space inserted for indentation; when zero the 'tabstop' value will be used
+vim.opt.showcmd = false
+vim.opt.showmode = false
+vim.opt.showtabline = 2
+vim.opt.sidescrolloff = 8
+vim.opt.signcolumn = 'yes:2'
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.smoothscroll = true
+vim.opt.softtabstop = -1
+vim.opt.splitbelow = true
+vim.opt.splitkeep = 'screen'
+vim.opt.splitright = true
+vim.opt.statuscolumn = '%s%=%l%{%v:virtnum == 0 && foldlevel(v:lnum) > foldlevel(v:lnum-1) ? "%C" : " "%}'
+vim.opt.swapfile = false
+vim.opt.synmaxcol = 1000
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 500
+vim.opt.title = true
+vim.opt.ttimeoutlen = 25
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 300
+vim.opt.virtualedit = 'block'
+vim.opt.wildmenu = true
+vim.opt.wildmode = { 'noselect', 'full' }
+vim.opt.wildoptions = { 'fuzzy', 'pum', 'tagfile' }
+vim.opt.winborder = 'none'
+vim.opt.wrap = false
+vim.opt.writebackup = false
+
+-- https://github.com/neovim/neovim/issues/28384
+-- if vim.fn.executable('C:/Program Files/Git/usr/bin/bash.exe') == 1 then
+--   vim.env.TMP = '/tmp'
+--   vim.opt.shell = '"C:/Program Files/Git/usr/bin/bash.exe"'
+--   vim.opt.shellcmdflag = '-c'
+--   vim.opt.shellpipe = '2>&1 | tee'
+--   vim.opt.shellquote = ''
+--   vim.opt.shellredir = '>%s 2>&1'
+--   vim.opt.shellslash = true
+--   vim.opt.shelltemp = false
+--   vim.opt.shellxescape = ''
+--   vim.opt.shellxquote = ''
+-- end
+
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+vim.keymap.set(
+  'n',
+  '<C-Left>',
+  '"<Cmd>vertical resize -" . v:count1 . "<CR>"',
+  { expr = true, replace_keycodes = false, desc = 'Decrease window width' }
+)
+vim.keymap.set(
+  'n',
+  '<C-Down>',
+  '"<Cmd>resize -" . v:count1 . "<CR>"',
+  { expr = true, replace_keycodes = false, desc = 'Decrease window height' }
+)
+vim.keymap.set(
+  'n',
+  '<C-Up>',
+  '"<Cmd>resize +" . v:count1 . "<CR>"',
+  { expr = true, replace_keycodes = false, desc = 'Increase window height' }
+)
+vim.keymap.set(
+  'n',
+  '<C-Right>',
+  '"<Cmd>vertical resize +" . v:count1 . "<CR>"',
+  { expr = true, replace_keycodes = false, desc = 'Increase window width' }
+)
+
+vim.api.nvim_create_augroup('custom-config', {})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = 'custom-config',
+  pattern = '*',
+  callback = function()
+    vim.opt_local.formatoptions = 'jqlnM'
+  end,
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = 'custom-config',
+  callback = function()
+    vim.hl.on_yank({ timeout = 80 })
+  end,
+})
+
+vim.api.nvim_create_autocmd('CmdlineChanged', {
+  group = 'custom-config',
+  pattern = { ':', '/', '?' },
+  callback = function()
+    vim.fn.wildtrigger()
+  end,
+})
+
+vim.cmd('colorscheme retrobox')
