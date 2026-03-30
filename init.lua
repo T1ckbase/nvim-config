2d31e15e0b27489f36c873606a53cc8ee2e69711
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2026-03-30 09:24:19 +0800

    Add hlsl

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
 
-vim.filetype.add({ pattern = { ['%.gitconfig%-.*'] = 'gitconfig' } })
+vim.filetype.add({
+  extension = {
+    effect = 'hlsl',
+  },
+  pattern = {
+    ['%.gitconfig%-.*'] = 'gitconfig',
+  },
+})
 
 vim.api.nvim_create_augroup('custom-config', {})
