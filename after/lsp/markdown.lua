ac41b87c58883e42c6588880a477c3931b624e70
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2026-05-18 13:49:47 +0800

    chore: switch to my package

--- after/lsp/markdown.lua
+++ after/lsp/markdown.lua
@@ -1,7 +1,8 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { 'deno', 'run', '--quiet', '--no-lock', '--allow-all', 'npm:vscode-markdown-languageserver/dist/node/workerMain.js', '--stdio' },
-  cmd_env = { NO_COLOR = true },
+  cmd = { 'pnpm', 'dlx', '--silent', '--package=@t1ckbase/vscode-langservers-extracted@latest', 'vscode-markdown-language-server', '--stdio' },
+  -- cmd = { 'deno', 'run', '--quiet', '--no-lock', '--allow-all', 'npm:vscode-markdown-languageserver/dist/node/workerMain.js', '--stdio' },
+  -- cmd_env = { NO_COLOR = true },
   filetypes = { 'markdown' },
   root_markers = { '.git' },
 }
